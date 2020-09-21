b43080a36da8351f18c95cb4555baa363ce848c6
zCranking <71561508+zCranking@users.noreply.github.com>
2020-09-20 18:33:47 -0700

    Add files via upload

---
+++ Trivia Quiz.py
@@ -0,0 +1,281 @@
+# I didn't how to do this but I knew you could do this so I searched it up it really helped me even though you didn't really teach us this
+name1=2569
+print ("Hello my name is Bot",name1,"!")
+import time
+time.sleep(0.5)
+print ("What is your name?")
+name2=input()
+print ("Nice too meet you!",name2)
+import time
+time.sleep(0.5)
+print ("So as you know I will be asking you some questions and I would like you to answer them. Does that sound good?(yes/no)")
+answer1=input()
+if answer1== ("Good") or answer1== ("good"):
+    print ("Good then let's begin")
+elif answer1== ("Yes") or answer1== ("yes"):
+    print ("Good then let's jump right in!")
+elif answer1== ("no") or answer1== ("No"):
+    print ("Umm then you could just leave by killing the code. I guess this is where we say goodbye?")
+else:
+    print (" I will take that as a yes becuase I am guessing there is a reason you clicked on this")
+print ("We will be asking you 10 questions")
+import time
+time.sleep(0.5)
+print ("These question will be different types of questions")
+import time
+time.sleep(0.5)
+print ("So you might be good at some and some you might not")
+import time
+time.sleep(0.5)
+print ("Test starting")
+import time
+time.sleep(0.5)
+print ("3")
+import time
+time.sleep(1)
+print ("2")
+import time
+time.sleep(1)
+print ("1")
+import time
+time.sleep(1)
+print ("First question")
+import time
+time.sleep(0.1)
+print ("Who is the current president?")
+import time
+time.sleep(0.1)
+print ("Don't forget you should make sure your spelling is correct!")
+#Question number 1
+president1=input ()
+if president1== ("Donald Trump") or president1== ("donald trump") or president1== ("Trump") or president1== ("Donald"):
+    print ("Good Job! You got it correct")
+else:
+    print ("Sorry but you got this question wrong")
+print ("I think you should be able to get the other ones correct")
+import time
+time.sleep(0.5)
+print ("Then next one will be for those gamers out there who want to test their knowledge")
+import time
+time.sleep(0.5)
+print ("There will be another question like this but this one is about Fortnite!")
+import time
+time.sleep(0.5)
+print ("I am sure a lot of have you have either played or at least heard of this game")
+import time
+time.sleep(0.5)
+print ("So know for the question")
+import time
+time.sleep(0.5)
+print ("What was one of the tier one skins in season 9?")
+#Question number 2
+skin=input()
+if skin== ("Rox") or skin== ("rox"):
+    print ("Correct")
+elif skin== ("Sentinel") or skin== ("sentinel"):
+    print ("Nice Job!")
+else:
+    print ("Sorry but you got it wrong")
+print ("Know let's go onto the next question")
+import time
+time.sleep(0.5)
+print ("This one is for them math geeks!")
+import time
+time.sleep(0.5)
+print ("What is the value of g in this eqaution?")
+import time
+time.sleep(0.5)
+print ("13 x g = 78")
+import time
+time.sleep(0.5)
+print ("If you need help ask your mom or dad")
+import time
+time.sleep(0.5)
+print ("Or if you want just use a calculator as this might be hard for some of you")
+#Question number 3
+math=input()
+if math== ("6") or math== ("six"):
+    print ("That is the correct answer!")
+elif math== ("7") or math== ("seven"):
+    print ("So close you almost got it")
+elif math== ("8") or math== ("eight"):
+    print ("You almost got it correct!")
+else:
+    print ("Sorry but you got this question wrong")
+print ("Ok now time to go onto the next question")
+import time
+time.sleep(0.5)
+print ("This question will be about history")
+import time
+time.sleep(0.5)
+print ("This history question won't be too hard but it will be American History")
+import time
+time.sleep(0.5)
+print ("Think of that as your hint becuase you are going to need it!")
+import time
+time.sleep(0.5)
+print ("So the question is...")
+import time
+time.sleep(0.2)
+print ("Who was the 1st president and who was the 16th president")
+import time
+time.sleep(0.5)
+print ("Type your answer like this: Name and Name (The name is the answers)")
+import time
+time.sleep(0.5)
+print ("Make sure that your spelling and capitalization is correct")
+#Question number 4
+president2=input()
+if president2== ("George Washington and Abraham Lincoln"):
+    print ("That is correct")
+else:
+    print ("Sorry that is incorrect")
+print ("Now for our next question")
+import time
+time.sleep(0.2)
+print ("This is our 5th question so we are halfway!")
+import time
+time.sleep(0.2)
+print ("What was the name of the person who created the company called Ford")
+#Question number 5
+FordCreator= input()
+if FordCreator== ("Henry Ford") or FordCreator== ("henry ford"):
+    print ("That is correct")
+else:
+    print("You are wrong")
+print (" Know onto our next question")
+import time
+time.sleep(0.5)
+print ("So the next question is")
+import time
+time.sleep(0.5)
+print ("Who is the author of the famous book")
+import time
+time.sleep(0.2)
+print ("Diary of a Wimpy Kid")
+#Question number 6
+wimpyKid= input ()
+if wimpyKid== ("Jeff Kinney") or wimpyKid== ("jeff kinney"):
+    print ("That is correct")
+else:
+    print ("Sorry that is incorrect")
+print ("I don't think that question should be hard")
+import time
+time.sleep(0.5)
+print ("I am pretty sure that most of us taking this trivia has already read the book")
+import time
+time.sleep(0.2)
+print ("Now onto our next question ")
+import time
+time.sleep(0.2)
+print ("What month was the first caronavirus case detected")
+#Question number 7
+caronavirus= input()
+if caronavirus== ("February") or caronavirus== ("february"):
+    print ("That is correct")
+else:
+    print("Sorry but that is incorrect")
+print ("Now let's go onto our next question!")
+import time
+time.sleep(0.2)
+print ("This one is about fortnite")
+import time
+time.sleep(0.5)
+print ("Ok, the question is the bounce pad still in the game? (yes/no)")
+#Question number 8
+bouncePad= input()
+if bouncePad== ("yes"):
+    print ("Correct")
+else:
+    print ("Sorry but that is correct")
+print ("Ok let's go onto our next question")
+import time
+time.sleep(0.5)
+print ("This question is about kids book")
+import time
+time.sleep(0.5)
+print ("This book is quite famous")
+import time
+time.sleep(0.5)
+print ("Fill in the blank")
+import time
+time.sleep(0.5)
+print ("Oh, ___ Places You'll __")
+import time
+time.sleep(0.5)
+print ("Type the first missing word")
+#Question number 9
+firstWord= input()
+if firstWord== ("the") or firstWord== ("The"):
+    print ("That is correct")
+else:
+    print("Sorry you got the first word incorrect")
+print ("Now type the second word")
+import time
+time.sleep(0.2)
+print ("Here is the hint")
+import time
+time.sleep(0.2)
+print ("It would have been easier if you got the first one")
+import time
+time.sleep(0.5)
+print ("Oh, ___ Places You'll __")
+secondWord= input()
+if secondWord== ("Go") or secondWord== ("go"):
+    print ("You got it correct")
+else:
+    print ("Sorry you got it wrong")
+print ("Ok that might have been hard if you didn't get the first part")
+import time
+time.sleep(0.5)
+print ("But don't worry let's just go onto the next question")
+import time
+time.sleep(0.5)
+print ("So this next question will be about what you did")
+import time
+time.sleep(0.2)
+print ("Did you use code fresh in the fortnite item shop!? (yes/no)")
+#Question number 10
+fresh= input()
+if fresh== ("yes") or fresh== ("Yes"):
+    print ("Good Job")
+elif fresh== ("no") or fresh== ("No"):
+    print ("This message should not have shown becuase that means you typed no!!!")
+else:
+    print (" I think you didn't understand it?")
+print ("Sorry but I just had to check")
+import time
+time.sleep(0.2)
+print ("Now let me tell you the last question")
+import time
+time.sleep(0.2)
+print ("So the last question is how would you rate this 1-10?")
+rating= input()
+if rating== ("10"):
+    print ("Thank you for your response")
+elif rating== ("9"):
+    print ("Thank you for your response")
+elif rating== ("8"):
+    print ("Thank you for your response")
+elif rating== ("7"):
+    print ("Thank you for your response")
+elif rating== ("6"):
+    print ("Thank you for your response")
+elif rating== ("5"):
+    print ("Thank you for your response")
+elif rating== ("4"):
+    print ("Thank you for your response")
+elif rating== ("3"):
+    print ("Thank you for your response")
+elif rating== ("2"):
+    print ("Thank you for your response")
+elif rating== ("1"):
+    print ("Thank you for your response")
+else:
+    print ("You didn't rate it 1-10")
+    import time
+time.sleep(0.8)
+print ("You have reached the end of the quiz!")
+import time
+time.sleep(0.8)
+print ("BYE!")
